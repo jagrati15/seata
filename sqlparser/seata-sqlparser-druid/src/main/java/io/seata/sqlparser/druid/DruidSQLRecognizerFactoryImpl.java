3ecfbc84011456157fc6e116524c20c6a1804748
jagrati15 <59571214+jagrati15@users.noreply.github.com>
2020-10-28 18:18:57 +0530

    Update DruidSQLRecognizerFactoryImpl.java

--- sqlparser/seata-sqlparser-druid/src/main/java/io/seata/sqlparser/druid/DruidSQLRecognizerFactoryImpl.java
+++ sqlparser/seata-sqlparser-druid/src/main/java/io/seata/sqlparser/druid/DruidSQLRecognizerFactoryImpl.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.sqlparser.druid;
+/*package io.seata.sqlparser.druid;
 
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
