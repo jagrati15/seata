46041e86e331570e3f4dff59b2570a1f68ea252b
jagrati15 <59571214+jagrati15@users.noreply.github.com>
2020-11-03 13:33:52 +0530

    Update MySQLInsertRecognizerTest.java

--- sqlparser/seata-sqlparser-druid/src/test/java/io/seata/sqlparser/druid/MySQLInsertRecognizerTest.java
+++ sqlparser/seata-sqlparser-druid/src/test/java/io/seata/sqlparser/druid/MySQLInsertRecognizerTest.java
@@ -13,7 +13,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.sqlparser.druid;
+package com.alibaba.druid.sql.dialect.mysql.visitor;
+/*package io.seata.sqlparser.druid;*/
 
 import java.util.Arrays;
 import java.util.Collections;
